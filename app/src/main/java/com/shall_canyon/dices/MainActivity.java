876a4f00b9d837c1f12431a6341d3e53acee8cae
ShallCanyon <37247435+ShallCanyon@users.noreply.github.com>
2019-09-03 16:52:07 +0800

    add FABs with different dices selection function add several icons to fit the FABs' icons add shadow of buttons adjust some components' positions

--- app/src/main/java/com/shall_canyon/dices/MainActivity.java
+++ app/src/main/java/com/shall_canyon/dices/MainActivity.java
@@ -6,22 +6,92 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+/**
+ * @author ShallCanyon
+ * @version 1.01
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    //determine whether the main fab_menu is opened or not
+    boolean isFabOpen = false;
+    //define maximum randomized data
+    int MAX = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * randomize data with a button
+         */
         final TextView tx_disp = findViewById(R.id.Text_Disp);
         Button bt_roll = findViewById(R.id.bt_roll);
         bt_roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int i = (int)(Math.random()*100 + 1);
+                int i = (int) (Math.random() * MAX + 1);
                 tx_disp.setText(String.valueOf(i));
             }
         });
+
+        /**
+         * fabs of dices selection
+         */
+        FloatingActionButton fab = findViewById(R.id.bt_fab);
+        final FloatingActionButton d6 = findViewById(R.id.bt_d6);
+        final FloatingActionButton d60 = findViewById(R.id.bt_d60);
+        final FloatingActionButton d100 = findViewById(R.id.bt_d100);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Snackbar.make(view,"own action",Snackbar.LENGTH_LONG)
+                //        .setAction("Action",null).show();
+                if (isFabOpen) {
+                    isFabOpen = false;
+                    d6.hide();
+                    d60.hide();
+                    d100.hide();
+                } else {
+                    isFabOpen = true;
+                    d6.show();
+                    d60.show();
+                    d100.show();
+                }
+            }
+        });
+
+        d6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MAX = 6;
+                tx_disp.setText(String.valueOf(6));
+                Toast.makeText(MainActivity.this, "D6", Toast.LENGTH_SHORT).show();
+            }
+        });
+        d60.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MAX = 60;
+                tx_disp.setText(String.valueOf(60));
+                Toast.makeText(MainActivity.this, "D60", Toast.LENGTH_SHORT).show();
+            }
+        });
+        d100.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MAX = 100;
+                tx_disp.setText(String.valueOf(100));
+                Toast.makeText(MainActivity.this, "D100", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
